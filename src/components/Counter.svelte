bdf9a40c2762418c3c7746f5c4bbcfe6be3c47d1
pdina <paolo@php3.it>
2022-06-10 01:27:31 +0200

    working base example only astro

---
+++ src/components/Counter.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let count = 0;
+
+    const handleClick = (e) => {
+        count += 1;
+    };
+</script>
+
+<h1>Svelte</h1>
+<button
+    style="padding: 20px 40px; background: blue; color: white"
+    on:click={handleClick}>{count}</button
+>
